2a830369181d3c5bd1846d7088e528e8f0e41062
Borja Elizalde <elizalde.borja@bcg.com>
2023-05-26 14:38:54 +0200

    added memers with missing linkedin

--- members_automation/src/tasks/linkedin_parse/lib/etl.py
+++ members_automation/src/tasks/linkedin_parse/lib/etl.py
@@ -5,12 +5,12 @@ from typing import List
 def preprocess_members(df_spreadsheet_members: pd.DataFrame) -> pd.DataFrame:
 
     df = df_spreadsheet_members.copy()#.rename(columns=dict_rename, inplace=False)
-    df.loc[df["LinkedIn"] == "", "LinkedIn"] = pd.NA
     df.loc[df["Name"] == "", "Name"] = pd.NA
     df = df[~df["Name"].isna()]
 
     df["Name"] = df["Name"].str.title()
     df["LinkedIn"] = update_url(df["LinkedIn"])
+    df.loc[df["LinkedIn"] == "", "LinkedIn"] = pd.NA
 
     df = df[df.columns[~df.columns.str.contains("Unnamed")]]
 
@@ -30,7 +30,7 @@ def postprocess_web_data(df_csv_web: pd.DataFrame, df_team_parsed: pd.DataFrame)
 
 def update_url(col_linkedin: pd.Series):
 
-    col_linkedin[~col_linkedin.str.startswith("http")] = "https://" + col_linkedin
+    col_linkedin[~(col_linkedin.str.startswith("http").astype(bool))] = "https://" + col_linkedin
     col_linkedin = col_linkedin.str.strip().str.replace(" ", "")
     col_linkedin = col_linkedin.str.replace("https://es.linkedin.com", "https://www.linkedin.com", regex=False)
     col_linkedin = col_linkedin.str.replace("https://linkedin.com", "https://www.linkedin.com", regex=False)
@@ -43,8 +43,8 @@ def preprocess_csv_web(df_csv_web: pd.DataFrame):
     df_csv_web = df_csv_web[~df_csv_web["Name"].isna()]
     df_csv_web["Name"] = df_csv_web["Name"].str.title()
 
-    df_csv_web.loc[df_csv_web["LinkedIn"] == "", "LinkedIn"] = pd.NA
     df_csv_web["LinkedIn"] = update_url(df_csv_web["LinkedIn"])
+    df_csv_web.loc[df_csv_web["LinkedIn"] == "", "LinkedIn"] = pd.NA
 
     df_csv_web["timestamp_creation"] = df_csv_web["Year"].astype(str) + "-" + df_csv_web["Start Date"]
     df_csv_web["timestamp_creation"] = pd.to_datetime(df_csv_web["timestamp_creation"]).astype(str)
