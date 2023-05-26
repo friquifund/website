7fae92057cf50f9a779f1411799baf629e46803c
Borja Elizalde <elizalde.borja@bcg.com>
2023-05-26 17:51:22 +0200

    changed pipeline name, refactored etl to make it cleaner

--- members_automation/src/tasks/linkedin_parse/lib/etl.py
+++ members_automation/src/tasks/linkedin_parse/lib/etl.py
@@ -9,31 +9,20 @@ def preprocess_members(df_spreadsheet_members: pd.DataFrame) -> pd.DataFrame:
     df = df[~df["Name"].isna()]
 
     df["Name"] = df["Name"].str.title()
-    df["LinkedIn"] = update_url(df["LinkedIn"])
-    df.loc[df["LinkedIn"] == "", "LinkedIn"] = pd.NA
+    df["LinkedIn"] = clean_url(df["LinkedIn"])
 
     df = df[df.columns[~df.columns.str.contains("Unnamed")]]
 
     return df
 
 
-def postprocess_web_data(df_csv_web: pd.DataFrame, df_team_parsed: pd.DataFrame) -> pd.DataFrame:
-
-    df_team_parsed["Title"] = df_team_parsed["Title"].str.replace("-", "|")
-    df_team_parsed["Title"] = df_team_parsed["Title"].str.split("|").str[1:].str.join("|").str.replace("linkedin", "")
-    df_team = pd.concat([df_csv_web[~df_csv_web["LinkedIn"].isin(df_team_parsed["LinkedIn"])], df_team_parsed], axis=0)
-    df_team = df_team.sort_values("Membership Number", ascending=True)
-    #df_team["leadership"] = int(0)
-    #df_team.loc[df_team["name"].isin(leadership_members), "leadership"] = int(1)
-    return df_team
-
-
-def update_url(col_linkedin: pd.Series):
+def clean_url(col_linkedin: pd.Series):
 
     col_linkedin[~(col_linkedin.str.startswith("http").astype(bool))] = "https://" + col_linkedin
     col_linkedin = col_linkedin.str.strip().str.replace(" ", "")
     col_linkedin = col_linkedin.str.replace("https://es.linkedin.com", "https://www.linkedin.com", regex=False)
     col_linkedin = col_linkedin.str.replace("https://linkedin.com", "https://www.linkedin.com", regex=False)
+    col_linkedin[col_linkedin == ""] = pd.NA
     return col_linkedin
 
 
@@ -43,8 +32,7 @@ def preprocess_csv_web(df_csv_web: pd.DataFrame):
     df_csv_web = df_csv_web[~df_csv_web["Name"].isna()]
     df_csv_web["Name"] = df_csv_web["Name"].str.title()
 
-    df_csv_web["LinkedIn"] = update_url(df_csv_web["LinkedIn"])
-    df_csv_web.loc[df_csv_web["LinkedIn"] == "", "LinkedIn"] = pd.NA
+    df_csv_web["LinkedIn"] = clean_url(df_csv_web["LinkedIn"])
 
     df_csv_web["timestamp_creation"] = df_csv_web["Year"].astype(str) + "-" + df_csv_web["Start Date"]
     df_csv_web["timestamp_creation"] = pd.to_datetime(df_csv_web["timestamp_creation"]).astype(str)
@@ -58,3 +46,14 @@ def preprocess_csv_web(df_csv_web: pd.DataFrame):
     df_csv_web = df_csv_web[df_csv_web.columns[~df_csv_web.columns.str.contains("Unnamed")]]
 
     return df_csv_web
+
+
+def postprocess_web_data(df_csv_web: pd.DataFrame, df_team_parsed: pd.DataFrame) -> pd.DataFrame:
+
+    df_team_parsed["Title"] = df_team_parsed["Title"].str.replace("-", "|")
+    df_team_parsed["Title"] = df_team_parsed["Title"].str.split("|").str[1:].str.join("|").str.replace("linkedin", "")
+    df_team = pd.concat([df_csv_web[~df_csv_web["LinkedIn"].isin(df_team_parsed["LinkedIn"])], df_team_parsed], axis=0)
+    df_team = df_team.sort_values("Membership Number", ascending=True)
+    #df_team["leadership"] = int(0)
+    #df_team.loc[df_team["name"].isin(leadership_members), "leadership"] = int(1)
+    return df_team
